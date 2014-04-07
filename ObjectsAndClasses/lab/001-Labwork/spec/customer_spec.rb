d0f23cbd1d031f29cdb13f61264df0a48719980b
stujo <wordpress@imagescript.com>
2014-04-06 18:50:31 -0700

    Another try at lab steps in RSpec

--- ObjectsAndClasses/lab/001-Labwork/spec/customer_spec.rb
+++ ObjectsAndClasses/lab/001-Labwork/spec/customer_spec.rb
@@ -1,111 +1,120 @@
 require 'spec_helper'
 
+check_class_defined(:Customer)
 
-if check_class_defined(:Customer)
-  describe Customer do
-    it "should be a class" do
+describe Customer do
+  describe  "should be a class and have an initialize method with 4 arguments", labStep: "Customer Basics" do
+    it "and should be a class" do
       expect(Customer).to respond_to(:new)
     end
+    it "and should have an initialize method with 4 arguments" do
+      expect(Customer.new('Jill', 'Smith', Date.new(1923, 1, 1), 0)).to be_a(Customer)
+    end
+  end
 
-    it "should have an initialize method with 4 arguments" do
-      expect(Customer.new).to respond_to(:initialize).with(4).arguments
+  describe "should have object oriented versions of the methods in the lib/customer_hash.rb StarterApp file ", labStep: "Customer Methods" do
+    before :all do
+      @emptyCustomer = Customer.new('Mary', 'Jones', Date.new(1987, 1, 1), 0)
     end
-    it "should have full_name" do
-      expect(Customer.new).to respond_to(:full_name).with(0).arguments
+    it "and should have full_name method defined with no arguments" do
+      expect(@emptyCustomer).to respond_to(:full_name).with(0).arguments
     end
-    it "should have age" do
-      expect(Customer.new).to respond_to(:age).with(0).arguments
+    it "and should have age method defined with no arguments" do
+      expect(@emptyCustomer).to respond_to(:age).with(0).arguments
     end
-    it "should have available_credit" do
-      expect(Customer.new).to respond_to(:available_credit).with(0).arguments
+    it "and should have available_credit method defined with no arguments" do
+      expect(@emptyCustomer).to respond_to(:available_credit).with(0).arguments
     end
-    it "should have use_credit" do
-      expect(Customer.new).to respond_to(:use_credit).with(1).arguments
+    it "and should have use_credit method with 1 argument" do
+      expect(@emptyCustomer).to respond_to(:use_credit).with(1).arguments
     end
-    it "should have attempt_credit_purchase" do
-      expect(Customer.new).to respond_to(:attempt_credit_purchase).with(2).arguments
+    it "and should have attempt_credit_purchase method with 2 arguments" do
+      expect(@emptyCustomer).to respond_to(:attempt_credit_purchase).with(2).arguments
     end
-    it "should have purchase_history" do
-      expect(Customer.new).to respond_to(:purchase_history).with(0).arguments
+    it "and should have purchase_history method defined with no arguments" do
+      expect(@emptyCustomer).to respond_to(:purchase_history).with(0).arguments
     end
+  end
 
-    describe "Should copy the methods in the lib/customer_hash.rb StarterApp file" do
-      before :all do
-        @birthyear = 1982
-        @customer = Customer.new('Bob', 'Smith', Date.new(@birthyear, 9, 2), 2000)
-        @ageToCheck = Time.now.year - @birthyear
-      end
-      it "should have a first_name of Bob" do
-        @customer.first_name.should eq 'Bob'
-      end
-      it "should have a last_name of Smith" do
-        @customer.last_name.should eq 'Smith'
-      end
-      it "should have available_credit of 2000" do
-        @customer.available_credit.should eq 2000
-      end
-      it "should have age of of #{@ageToCheck}" do
-        @customer.age.should eq @ageToCheck
-      end
+  describe "Should copy the methods in the lib/customer_hash.rb StarterApp file", labStep: "Customer Method Implementations"  do
+    before :all do
+      @birthyear = 1982
+      @customer = Customer.new('Bob', 'Smith', Date.new(@birthyear, 9, 2), 2000)
+      @ageToCheck = Time.now.year - @birthyear
     end
-
-    describe "#use_credit (should take one parameter)" do
-      before :each do
-        @customer = Customer.new('Molly', 'Jones', Date.new(1992, 9, 2), 1500)
-      end
-      it "should decrement the available_credit if available" do
-        @customer.use_credit 100
-        @customer.available_credit.should eq 1400
-      end
-      it "should return true if credit was available" do
-        @customer.use_credit 300.should eq true
-      end
-      it "should NOT decrement the available_credit if available" do
-        @customer.use_credit 2000
-        @customer.available_credit.should eq 1500
-      end
-      it "should return false if insufficient credit available" do
-        (@customer.use_credit 1501).should eq false
-      end
+    it "and should have a first_name of Bob" do
+      @customer.first_name.should eq 'Bob'
     end
-
-    describe "#attempt_credit_purchase (should take two parameters)" do
-      before :each do
-        @customer = Customer.new('Sally', 'Johnson', Date.new(1987, 11, 3), 600)
-      end
-      it "should return true if credit was available" do
-        (@customer.attempt_credit_purchase 123, "Bookcase").should eq true
-      end
-      it "should return false if credit was not available" do
-        (@customer.attempt_credit_purchase 601, "iPhone").should eq false
-      end
-      it "should add description to the purchase history if credit was available" do
-        @customer.attempt_credit_purchase 599, "Necklace"
-        @customer.purchase_history.should array_include "Necklace"
-      end
-      it "should not add description to the purchase history if credit was not available" do
-        history = @customer.purchase_history
-        @customer.attempt_credit_purchase 10000, "Ford Focus"
-        @customer.purchase_history.should eq history
-      end
+    it "and should have a last_name of Smith" do
+      @customer.last_name.should eq 'Smith'
     end
+    it "and should have available_credit of 2000" do
+      @customer.available_credit.should eq 2000
+    end
+    it "and should have age of of #{@ageToCheck}" do
+      @customer.age.should eq @ageToCheck
+    end
+    it "and should have a full_name of Bob Smith" do
+      @customer.last_name.should eq 'Bob Smith'
+    end
+  end
 
-    describe "#purchase_history (should take no parameters)" do
-      before :each do
-        @customer = Customer.new('Bob', 'Holly', Date.new(1965, 1, 1), 6000)
-      end
-      it "should return an empty array for new customers" do
-        @customer.purchase_history.should eq []
-      end
-      it "should return an array with one element after 1 purchase" do
-        @customer.attempt_credit_purchase 3, "Birthday Card"
-        @customer.purchase_history.should eq ["Birthday Card"]
-      end
-      it "should return an array with two elements after 2 purchases" do
-        @customer.attempt_credit_purchase 4, "Valentine Card"
-        @customer.attempt_credit_purchase 4, "Valentine Card"
-        @customer.purchase_history.should eq ["Valentine Card", "Valentine Card"]
-      end
+  describe "#use_credit (should take one parameter)", labStep: "Customer Credit Processing" do
+    before :each do
+      @customer = Customer.new('Molly', 'Jones', Date.new(1992, 9, 2), 1500)
+    end
+    it "and should decrement the available_credit if available" do
+      @customer.use_credit 100
+      @customer.available_credit.should eq 1400
+    end
+    it "and should return true if credit was available" do
+      @customer.use_credit 300.should eq true
+    end
+    it "and should NOT decrement the available_credit if available" do
+      @customer.use_credit 2000
+      @customer.available_credit.should eq 1500
+    end
+    it "and should return false if insufficient credit available" do
+      (@customer.use_credit 1501).should eq false
+    end
+  end
+
+  describe "#attempt_credit_purchase (should take two parameters)", labStep: "Customer Purchases" do
+    before :each do
+      @customer = Customer.new('Sally', 'Johnson', Date.new(1987, 11, 3), 600)
+    end
+    it "and should return true if credit was available" do
+      (@customer.attempt_credit_purchase 123, "Bookcase").should eq true
+    end
+    it "and should return false if credit was not available" do
+      (@customer.attempt_credit_purchase 601, "iPhone").should eq false
+    end
+    it "and should add description to the purchase history if credit was available" do
+      @customer.attempt_credit_purchase 599, "Necklace"
+      @customer.purchase_history.should array_include "Necklace"
+    end
+    it "and should not add description to the purchase history if credit was not available" do
+      history = @customer.purchase_history
+      @customer.attempt_credit_purchase 10000, "Ford Focus"
+      @customer.purchase_history.should eq history
+    end
+  end
+
+  describe "#purchase_history (should take no parameters)", labStep: "Customer Purchase History" do
+    before :each do
+      @customer = Customer.new('Bob', 'Holly', Date.new(1965, 1, 1), 6000)
+    end
+    it "and should return an empty array for new customers" do
+      @customer.purchase_history.should eq []
+    end
+    it "and should return an array with one element after 1 purchase" do
+      @customer.attempt_credit_purchase 3, "Birthday Card"
+      @customer.purchase_history.should eq ["Birthday Card"]
+    end
+    it "and should return an array with two elements after 2 purchases" do
+      @customer.attempt_credit_purchase 4, "Valentine Card"
+      @customer.attempt_credit_purchase 4, "Valentine Card"
+      @customer.purchase_history.should eq ["Valentine Card", "Valentine Card"]
     end
   end
 end
--- ObjectsAndClasses/lab/001-Labwork/spec/spec_helper.rb
+++ ObjectsAndClasses/lab/001-Labwork/spec/spec_helper.rb
@@ -2,11 +2,12 @@ require 'rspec'
 
 RSpec.configure do |config|
   config.mock_framework = :rspec
+  RSpec.configuration.color = true
 end
 
 def check_class_defined class_name
   class_file = "lib/#{class_name.downcase}.rb"
-  describe "#{class_name} class should exist" do
+  describe "#{class_name} class should exist", labStep: "#{class_name} Exists" do
     it "should be in #{class_file}" do
       expect(Object.const_defined?(class_name)).to(eq(true), "Class #{class_name} should be defined in '#{class_file}'")
       expect(Object.const_get(class_name)).to(be_a(Class), "#{class_name} should be a Class'")
@@ -18,7 +19,7 @@ end
 def check_module_defined class_name
   class_file = "lib/#{class_name.downcase}.rb"
   exists = false
-  describe "#{class_name} module should exist" do
+  describe "#{class_name} module should exist", labStep: "#{class_name} Exists" do
     it "should be in #{class_file}" do
       expect(Object.const_defined?(class_name)).to(eq(true), "Module #{class_name} should be defined in '#{class_file}'")
       expect(Object.const_get(class_name)).to(be_a(Module), "#{class_name} should be a Module'")
@@ -27,13 +28,101 @@ def check_module_defined class_name
   exists
 end
 
-#Require the files the students have written
-def load_lab_app_lib_files
-  libFiles = File.dirname(__FILE__) + "/../lib/*.rb"
-  Dir[libFiles].each {|file| require file}
+
+class LabTestProgressState
+  def initialize steps
+    @name = File.basename(File.dirname(File.dirname(__FILE__)))
+    @stepProgress = steps.map.with_index do |label, index|
+      {
+          label: label,
+          open: index == 0
+      }
+    end
+    @currentStep = 0
+    puts "======================================================="
+    puts "Checking your work for #{@name}"
+    puts "======================================================="
+    self.load_lab_app_lib_files
+    puts "======================================================="
+    puts ""
+    puts "Review the results of the RSpec tests below to see what"
+    puts "you should work on next"
+    puts "Always work on the first test to fail first"
+    puts ""
+    puts "======================================================="
+
+    labTestProgress = self
+    RSpec.configure do |config|
+      config.around(:each) do |proc|
+        labStepNameGroup = proc.metadata[:labStep]
+        if labStepNameGroup.nil? || labTestProgress.stepOpen(labStepNameGroup)
+          proc.run
+
+          ex = example.instance_variable_get("@exception")
+
+          if ex.nil?
+            #success only code goes here
+            if labStepNameGroup
+              labTestProgress.stepComplete(labStepNameGroup)
+            end
+          end
+        else
+          puts "Skipping #{labStepNameGroup}"
+        end
+      end
+    end
+
+  end
+
+  #Require the files the students have written
+  def load_lab_app_lib_files
+    libFiles = File.dirname(__FILE__) + "/../lib/*.rb"
+    Dir[libFiles].each do |file|
+      fullpath = File.expand_path(file)
+      puts "Loading your file #{fullpath}"
+      require fullpath
+    end
+  end
+
+  def findStep label
+    @stepProgress.each.with_index do |step, index|
+      return index if step[:label].eql? label
+    end
+    raise "Unknown Lab Step '#{label}''"
+  end
+
+
+  #set current step if open
+  def stepOpen stepLabel
+    @currentStep = findStep(stepLabel)
+    if @stepProgress[@currentStep][:open]
+      true
+    else
+      #skip tests
+      false
+    end
+  end
+
+  #Open up next step
+  def stepComplete stepName
+    nextStep = @stepProgress[@currentStep + 1]
+    nextStep[:open] = true unless nextStep.nil?
+  end
+
 end
 
-load_lab_app_lib_files
+LabTestProgress = LabTestProgressState.new [
+                                               "Customer Exists",
+                                               "Customer Basics",
+                                               "Customer Methods",
+                                               "Customer Method Implementations",
+                                               "Customer Credit Processing",
+                                               "Customer Purchases",
+                                               "Customer Purchase History",
+                                               "OrderProcessor Exists",
+                                           ]
+
+
 
 
 
